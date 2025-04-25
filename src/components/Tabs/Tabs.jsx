ea32b9c2ac071e4c96d90ac1bac6984a7e344851
Seva <20211206nch@mail.ru>
2025-04-25 18:56:36 +0300

    add activeClass using classNames lib

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,5 +1,6 @@
+import { useState } from 'react'
 import classNames from 'classnames'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 // import { useDispatch, useSelector } from 'react-redux'
 
 import { sortCheapest, sortFastest, sortOptimal } from '../../store/actions'
@@ -7,46 +8,60 @@ import { sortCheapest, sortFastest, sortOptimal } from '../../store/actions'
 import style from './Tabs.module.scss'
 
 function Tabs() {
+  const [activeButton, setActiveButton] = useState('btn-cheap')
   const dispatch = useDispatch()
   // const data = useSelector((state) => {
   //   // console.log('Tabs state > ', state.reducerSorter)
   // })
+  const { data } = useSelector((state) => state.reducerData)
 
   const handleClickCheap = () => {
-    dispatch(sortCheapest())
+    dispatch(sortCheapest(data))
+    setActiveButton('btn-cheap')
   }
   const handleClickFast = () => {
-    dispatch(sortFastest())
+    dispatch(sortFastest(data))
+    setActiveButton('btn-fast')
   }
   const handleClickOpt = () => {
-    dispatch(sortOptimal())
+    dispatch(sortOptimal(data))
+    setActiveButton('btn-opt')
   }
 
   return (
     <ul className={style.tabs}>
       <li className={style.tabs__item}>
         <button
+          id="btn-cheap"
           type="button"
           onClick={handleClickCheap}
-          className={classNames(style.tabs__btn, { [style.active]: true })}
+          className={classNames(style.tabs__btn, {
+            [style.active]: activeButton === 'btn-cheap',
+          })}
         >
           Самый дешевый
         </button>
       </li>
       <li className={style.tabs__item}>
         <button
+          id="btn-fast"
           type="button"
           onClick={handleClickFast}
-          className={style.tabs__btn}
+          className={classNames(style.tabs__btn, {
+            [style.active]: activeButton === 'btn-fast',
+          })}
         >
           Самый быстрый
         </button>
       </li>
       <li className={style.tabs__item}>
         <button
+          id="btn-opt"
           type="button"
           onClick={handleClickOpt}
-          className={style.tabs__btn}
+          className={classNames(style.tabs__btn, {
+            [style.active]: activeButton === 'btn-opt',
+          })}
         >
           Оптимальный
         </button>
