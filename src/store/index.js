44a5cc42d1aa0b5f6d3d321589d3c72e51915936
Seva <20211206nch@mail.ru>
2025-05-24 09:25:42 +0300

    deleted redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { thunk } from 'redux-thunk'
 
-import { reducer } from './reducers'
+import { reducer, loggerMiddleWare } from './reducers'
 
 export default configureStore({
   reducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+  middleware: (getDefaultMiddleware) =>
+    // eslint-disable-next-line implicit-arrow-linebreak
+    getDefaultMiddleware().concat(loggerMiddleWare),
+  // devTools: process.env.NODE_ENV !== 'production',
 })
