68b5cc39e245cb1323d33a080906711c1b5ecf31
Seva <20211206nch@mail.ru>
2025-05-25 19:42:39 +0300

    modified reducers, now only one

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import { reducer, loggerMiddleWare } from './reducers'
+import reducer from './reducers'
 
 export default configureStore({
   reducer,
-  middleware: (getDefaultMiddleware) =>
-    // eslint-disable-next-line implicit-arrow-linebreak
-    getDefaultMiddleware().concat(loggerMiddleWare),
-  // devTools: process.env.NODE_ENV !== 'production',
+  devTools: import.meta.env.VITE_MODE !== 'production',
 })
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,5 +1,8 @@
+/* eslint-disable operator-linebreak */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable prettier/prettier */
+/* eslint-disable indent */
 /* eslint-disable default-param-last */
-import { combineReducers } from '@reduxjs/toolkit'
 
 import {
   CHEAPEST_VARIANT,
@@ -14,163 +17,316 @@ import {
   FETCH_DATA_REQUEST,
   FETCH_DATA_SUCCESS,
   FETCH_DATA_FAILURE,
+  FETCH_DATA_ALL_LOADED,
 } from './type'
 
 const initialStateData = {
   loading: false,
+  allLoaded: false,
   error: null,
-  data: {},
+  data: { tickets: [], stop: false },
+  filter: {
+    all: true,
+    without: true,
+    one: true,
+    two: true,
+    three: true,
+  },
   displayedData: [],
+  dataFiltered: [],
+  sortType: '',
   currentPage: 0,
   pageSize: 5,
 }
+const calculateFilterState = (stateN) =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  [stateN.without, stateN.one, stateN.two, stateN.three].every(Boolean)
 
-export const reducerData = (state = initialStateData, action) => {
+const filterTickets = (tickets, filter) => {
+  if (!tickets) return []
+  if (filter.all) return tickets
+
+  return tickets.filter((ticket) => {
+    const { segments } = ticket
+    const transferCount = segments.map((segment) => segment.stops.length)
+
+    return (
+      (filter.without && transferCount.some((count) => count === 0)) ||
+      (filter.one && transferCount.some((count) => count === 1)) ||
+      (filter.two && transferCount.some((count) => count === 2)) ||
+      (filter.three && transferCount.some((count) => count === 3))
+    )
+  })
+}
+
+const updateDisplayedData = ({ data, currentPage, pageSize }) => [
+  ...data.slice(0, (currentPage + 1) * pageSize),
+]
+
+const sortCheapest = (data) => data.slice().sort((a, b) => a.price - b.price)
+
+const sortFastest = (data) =>
+  data.slice().sort((a, b) => a.segments[0].duration - b.segments[0].duration)
+
+const sortOptimal = (data) =>
+  data.slice().sort((a, b) => {
+    const restCostPerMinute = 20
+    const optVar1 = a.price + a.segments[0].duration * restCostPerMinute
+    const optVar2 = b.price + b.segments[0].duration * restCostPerMinute
+    return optVar1 - optVar2
+  })
+
+const getDataSorted = (sortType, filteredTickets) => {
+  switch (sortType) {
+    case 'cheapest':
+      return sortCheapest(filteredTickets)
+    case 'fastest':
+      return sortFastest(filteredTickets)
+    case 'optimal':
+      return sortOptimal(filteredTickets)
+    default:
+      return filteredTickets
+  }
+}
+
+const getDataFilteredSorted = (state, filter) => {
+  const filterIsChanged =
+    JSON.stringify(filter) === JSON.stringify(state.filter)
+
+  const filteredTickets = !filterIsChanged
+    ? filterTickets(state.data.tickets, filter)
+    : state.dataFiltered
+
+  const dataFilterSort = getDataSorted(state.sortType, filteredTickets)
+
+  return dataFilterSort
+}
+
+const reducer = (state = initialStateData, action) => {
   switch (action.type) {
     case CHEAPEST_VARIANT: {
-      if (!state.data?.tickets) return state
-      const newState = {
-        ...state,
+      if (!state.dataFiltered?.length) return state
+
+      const sortedTickets = sortCheapest(state.dataFiltered)
+      const displayedData = updateDisplayedData({
+        data: sortedTickets,
         currentPage: 0,
-        data: {
-          ...state.data,
-          tickets: [...state.data.tickets].sort((a, b) => a.price - b.price),
-        },
-      }
+        pageSize: state.pageSize,
+      })
 
       return {
-        ...newState,
-        displayedData: [
-          ...newState.data.tickets.slice(
-            0,
-            newState.pageSize * newState.currentPage + newState.pageSize
-          ),
-        ],
+        ...state,
+        sortType: 'cheapest',
+        currentPage: 0,
+        displayedData,
       }
     }
     case FASTEST_VARIANT: {
-      if (!state.data?.tickets) return state
+      if (!state.dataFiltered?.length) return state
 
-      const newState = {
-        ...state,
+      const sortedTickets = sortFastest(state.dataFiltered)
+      const displayedData = updateDisplayedData({
+        data: sortedTickets,
         currentPage: 0,
-        data: {
-          ...state.data,
-          tickets: [...state.data.tickets].sort(
-            (a, b) => a.segments[0].duration - b.segments[0].duration
-          ),
-        },
-      }
+        pageSize: state.pageSize,
+      })
+
       return {
-        ...newState,
-        displayedData: [
-          ...newState.data.tickets.slice(
-            0,
-            newState.pageSize * newState.currentPage + newState.pageSize
-          ),
-        ],
+        ...state,
+        sortType: 'fastest',
+        currentPage: 0,
+        displayedData,
       }
     }
     case OPTIMAL_VARIANT: {
-      if (!state.data?.tickets) return state
+      if (!state.dataFiltered?.length) return state
 
-      const newState = {
-        ...state,
+      const sortedTickets = sortOptimal(state.dataFiltered)
+      const displayedData = updateDisplayedData({
+        data: sortedTickets,
         currentPage: 0,
-        data: {
-          ...state.data,
-          tickets: [...state.data.tickets].sort((a, b) => {
-            const restCostPerMinute = 20
-            const optVar1 = a.price + a.segments[0].duration * restCostPerMinute
-            const optVar2 = b.price + b.segments[0].duration * restCostPerMinute
-            return optVar1 - optVar2
-          }),
-        },
-      }
+        pageSize: state.pageSize,
+      })
+
       return {
-        ...newState,
-        displayedData: [
-          ...newState.data.tickets.slice(
-            0,
-            newState.pageSize * newState.currentPage + newState.pageSize
-          ),
-        ],
+        ...state,
+        sortType: 'optimal',
+        currentPage: 0,
+        displayedData,
       }
     }
 
     case SHOW_MORE: {
-      if (!state.data?.tickets) return state
-      const newState = { ...state, currentPage: state.currentPage + 1 }
+      if (!state.dataFiltered?.length) return state
+      const newPage = state.currentPage + 1
+
+      const dataFilteredSorted = getDataFilteredSorted(state, state.filter)
+      const displayedData = updateDisplayedData({
+        data: dataFilteredSorted,
+        currentPage: newPage,
+        pageSize: state.pageSize,
+      })
+
       return {
-        ...newState,
-        displayedData: [
-          ...newState.data.tickets.slice(
-            0,
-            newState.pageSize * newState.currentPage + newState.pageSize
-          ),
-        ],
+        ...state,
+        currentPage: newPage,
+        displayedData,
       }
     }
 
     case FETCH_DATA_REQUEST:
       return { ...state, loading: true, error: null }
-    case FETCH_DATA_SUCCESS:
-      // return {
-      //   ...state,
-      //   loading: false,
-      //   data: {
-      //     tickets: [...state.data.tickets, ...action.payload.tickets],
-      //     stop: action.payload.stop,
-      //   },
-      // }
-      return { ...state, loading: false, data: action.payload }
-    case FETCH_DATA_FAILURE:
-      return { ...state, loading: false, error: action.payload }
-    default:
-      return state
-  }
-}
 
-const initialStateFilter = {
-  all: false,
-  without: false,
-  one: false,
-  two: false,
-  three: false,
-}
+    case FETCH_DATA_SUCCESS: {
+      const newTickets = [...action.payload.tickets]
+      const filteredTickets = filterTickets(newTickets, state.filter)
+      const sortedTickets = getDataSorted(state.sortType, filteredTickets)
 
-export const reducerFilter = (state = initialStateFilter, action) => {
-  const calculateAllState = (stateN) =>
-    // eslint-disable-next-line implicit-arrow-linebreak
-    [stateN.without, stateN.one, stateN.two, stateN.three].every(Boolean)
+      const displayedData = updateDisplayedData({
+        data: sortedTickets,
+        currentPage: state.currentPage,
+        pageSize: state.pageSize,
+      })
 
-  switch (action.type) {
-    case ALL_TRANSFER: {
-      const newAllState = !state.all
       return {
         ...state,
+        loading: false,
+        data: {
+          tickets: newTickets,
+          stop: action.payload.stop,
+        },
+        dataFiltered: sortedTickets,
+        displayedData,
+      }
+    }
+    case FETCH_DATA_ALL_LOADED:
+      return { ...state, allLoaded: true }
+    case FETCH_DATA_FAILURE:
+      return { ...state, loading: false, error: action.payload }
+
+    case ALL_TRANSFER: {
+      const newAllState = !state.filter.all
+      const newFilter = {
         all: newAllState,
         without: newAllState,
         one: newAllState,
         two: newAllState,
         three: newAllState,
       }
+
+      const dataFilteredSorted = getDataFilteredSorted(state, newFilter)
+      const displayedData = updateDisplayedData({
+        data: dataFilteredSorted,
+        currentPage: 0,
+        pageSize: state.pageSize,
+      })
+      return {
+        ...state,
+        loading: false,
+        filter: newFilter,
+        dataFiltered: dataFilteredSorted,
+        displayedData,
+      }
     }
     case WITHOUT_TRANSFER: {
-      const newState = { ...state, without: !state.without }
-      return { ...newState, all: calculateAllState(newState) }
+      const newFilter = { ...state.filter, without: !state.filter.without }
+      const newState = {
+        ...state,
+        filter: newFilter,
+      }
+
+      const dataFilteredSorted = getDataFilteredSorted(state, newFilter)
+      const displayedData = updateDisplayedData({
+        data: dataFilteredSorted,
+        currentPage: 0,
+        pageSize: state.pageSize,
+      })
+
+      return {
+        ...newState,
+        currentPage: 0,
+        filter: {
+          ...newState.filter,
+          all: calculateFilterState(newState.filter),
+        },
+        dataFiltered: dataFilteredSorted,
+        displayedData,
+      }
     }
     case ONE_TRANSFER: {
-      const newState = { ...state, one: !state.one }
-      return { ...newState, all: calculateAllState(newState) }
+      const newFilter = { ...state.filter, one: !state.filter.one }
+      const newState = {
+        ...state,
+        filter: newFilter,
+      }
+
+      const dataFilteredSorted = getDataFilteredSorted(state, newFilter)
+      const displayedData = updateDisplayedData({
+        data: dataFilteredSorted,
+        currentPage: 0,
+        pageSize: state.pageSize,
+      })
+
+      return {
+        ...newState,
+        currentPage: 0,
+        filter: {
+          ...newState.filter,
+          all: calculateFilterState(newState.filter),
+        },
+        dataFiltered: dataFilteredSorted,
+        displayedData,
+      }
     }
     case TWO_TRANSFER: {
-      const newState = { ...state, two: !state.two }
-      return { ...newState, all: calculateAllState(newState) }
+      const newFilter = { ...state.filter, two: !state.filter.two }
+      const newState = {
+        ...state,
+        filter: newFilter,
+      }
+
+      const dataFilteredSorted = getDataFilteredSorted(state, newFilter)
+      const displayedData = updateDisplayedData({
+        data: dataFilteredSorted,
+        currentPage: 0,
+        pageSize: state.pageSize,
+      })
+
+      return {
+        ...newState,
+        currentPage: 0,
+        filter: {
+          ...newState.filter,
+          all: calculateFilterState(newState.filter),
+        },
+        dataFiltered: dataFilteredSorted,
+        displayedData,
+      }
     }
     case THREE_TRANSFER: {
-      const newState = { ...state, three: !state.three }
-      return { ...newState, all: calculateAllState(newState) }
+      const newFilter = { ...state.filter, three: !state.filter.three }
+      const newState = {
+        ...state,
+        filter: newFilter,
+      }
+
+      const dataFilteredSorted = getDataFilteredSorted(state, newFilter)
+
+      const displayedData = updateDisplayedData({
+        data: dataFilteredSorted,
+        currentPage: 0,
+        pageSize: state.pageSize,
+      })
+      return {
+        ...newState,
+        currentPage: 0,
+        filter: {
+          ...newState.filter,
+          all: calculateFilterState(newState.filter),
+        },
+        dataFiltered: dataFilteredSorted,
+        displayedData,
+      }
     }
 
     default:
@@ -178,26 +334,4 @@ export const reducerFilter = (state = initialStateFilter, action) => {
   }
 }
 
-export const reducer = combineReducers({
-  reducerData,
-  reducerFilter,
-})
-
-export const loggerMiddleWare = (store) => (next) => (action) => {
-  // console.log('logger before > ', store.getState())
-  const result = next(action)
-  console.log('LOGGER reducerData after> ', store.getState()?.reducerData)
-  return result
-}
-
-// export const schedulerMiddleware = (store) => (next) => (action) => {
-//   const delayMW = action?.meta?.delay
-//   if (!delayMW) return next(action)
-
-//   const timerId = setTimeout(() => next(action), delayMW)
-
-//   return () => {
-//     console.log('clianing')
-//     clearTimeout(timerId)
-//   }
-// }
+export default reducer
